1104c20ce6bcaf1cbe992fe44f5fa2c6c4e6146a
Ellen Marin <ellenmarin@Air-de-Ellen.lan>
2024-04-30 21:59:49 +0100

    feat: add initial header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,5 +3,13 @@ import {Button} from "./ui/button";
 import { MenuIcon } from "lucide-react";
 
 const Header = () => {
-    
+    return(
+    <div className="flex justify-between pt-6 px-5">
+        <Image src="/logo.png" alt="FOOD" height={70} width={150}/>
+        <Button size="icon" variant="outline" className="border-none bg-transparent">
+            <MenuIcon />
+        </Button>
+    </div>
+    );
 }
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import Header from "./_components/header";
+
 const Home = () => {
   return (
-    <div className="bg-red-600 p-5">
-      <h1>Hello World</h1>
-    </div>
+    <Header />
   );
 };
 
